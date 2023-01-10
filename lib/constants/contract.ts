ab5b4082b66af315d59b4755195d218ad7b8b1bf
VandanRogheliya <vandan@daolens.com>
2023-01-10 21:07:58 +0530

    update contract

--- lib/constants/contract.ts
+++ lib/constants/contract.ts
@@ -75,18 +75,18 @@ export const CONTRACT_FUNCTION_NAME_MAP = {
 
 export const CONTRACTS: Record<ContractNameType, ContractType> = {
   workspace: {
-    address: '0x179b579b993D849Aa61Bd08d4f8CBd68Af1FF0FF',
-    polygonMumbaiAddress: '0x179b579b993D849Aa61Bd08d4f8CBd68Af1FF0FF',
+    address: '0xb5F1ce0D36b324C90B604644B8a81a6A53D9237F',
+    polygonMumbaiAddress: '0xb5F1ce0D36b324C90B604644B8a81a6A53D9237F',
     abi: WorkspaceContractAbi,
   },
   application: {
-    address: '0x6Cbf064830370777C049E263DaAA618560e4BDf9',
-    polygonMumbaiAddress: '0x6Cbf064830370777C049E263DaAA618560e4BDf9',
+    address: '0xa2FE45c6f31Eb09D287cf2b7De54DB1469b9F029',
+    polygonMumbaiAddress: '0xa2FE45c6f31Eb09D287cf2b7De54DB1469b9F029',
     abi: ApplicationContractAbi,
   },
   grant: {
-    address: '0x97d69B1E0B75204f8E147C31F5d71b61d0834788',
-    polygonMumbaiAddress: '0x97d69B1E0B75204f8E147C31F5d71b61d0834788',
+    address: '0xb31250fe989DBD2dbd7883A8b4C741A2ab9D46ad',
+    polygonMumbaiAddress: '0xb31250fe989DBD2dbd7883A8b4C741A2ab9D46ad',
     abi: GrantContractAbi,
   },
   // address will be the grantAddress of the grant
